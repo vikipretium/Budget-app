c08cc6a64f5dfde3ddd462d697616915af57ee6b
vikipretium <vighneshpasupathy@gmail.com>
2022-07-05 02:09:34 +0530

    add sign up and login page using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users do
   resources :groups
   resources :entities
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
     get '/splashscreen/index', to: 'splashscreen#index'
   # Defines the root path route ("/")
